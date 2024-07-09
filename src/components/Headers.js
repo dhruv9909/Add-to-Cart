eb1420151273dba92f2d60fbbeba5083a5e0fe4c
dhruv9909 <dhruvsachdeva951@gmail.com>
2024-07-10 01:30:05 +0530

    Fixing

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -5,24 +5,23 @@ import './Styel.css'
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-
 const Headers = () => {
-    const cartData = useSelector((state)=>state.allCart)
-    console.log(cartData)
+    const cartData = useSelector((state) => state.allCart);
+    const totalQuantity = cartData.carts.reduce((acc, item) => acc + item.qnty, 0);
+
     return (
         <>
             <Navbar style={{ height: "60px", background: "black", color: "white" }}>
-
                 <Container>
-                     <NavLink to = '/' className= "text-decoration-none text-light mx-2">
-                    <h3 className='text-light'>Ecommerce</h3>
+                    <NavLink to='/' className="text-decoration-none text-light mx-2">
+                        <h3 className='text-light'>Ecommerce</h3>
                     </NavLink>
-                    <NavLink to = '/cart' className= "text-decoration-none text-light mx-2">
-                    <div id='ex4'>
-                        <span className='p1 fa-stack fa-2x has-badge' data-count={cartData.carts.length}>
-                            <i class="fa-solid fa-cart-shopping"></i>
-                        </span>
-                    </div>
+                    <NavLink to='/cart' className="text-decoration-none text-light mx-2">
+                        <div id='ex4'>
+                            <span className='p1 fa-stack fa-2x has-badge' data-count={totalQuantity}>
+                                <i className="fa-solid fa-cart-shopping"></i>
+                            </span>
+                        </div>
                     </NavLink>
                 </Container>
             </Navbar>
@@ -30,4 +29,4 @@ const Headers = () => {
     )
 }
 
-export default Headers
+export default Headers;
